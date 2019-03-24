37353200df0bb2c9bac485ba9bc01fcb76748855
Gavin <gavinjboyd@gmail.com>
2019-03-24 18:56:06 +0900

    svg parser working

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,9 @@
+const parse = require('parse-svg-path');
+const extract = require('extract-svg-path');
 console.log("Hello world");
+getSVGData('./svgs/1.svg');
+function getSVGData(svgFileName) {
+    let path = extract(svgFileName);
+    let svg = parse(path);
+    console.log(svg);
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,1 +1,14 @@
+const parse = require('parse-svg-path');
+const extract = require('extract-svg-path');
+
 console.log("Hello world");
+getSVGData('./svgs/1.svg')
+getSVGData('./svgs/2.svg')
+
+function getSVGData(svgFileName:string){
+
+let path = extract(svgFileName)
+let svg = parse(path)
+console.log(svgFileName);
+console.log(svg);
+}
