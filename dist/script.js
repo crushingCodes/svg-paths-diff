98151716f95ac463bf960d305c4b34f2f526df84
Gavin <gavinjboyd@gmail.com>
2019-03-25 09:57:47 +0900

    console out individual path strings

--- dist/script.js
+++ dist/script.js
@@ -2,31 +2,29 @@
 const parse = require('parse-svg-path');
 const extract = require('extract-svg-path');
 //Global Variables
-let pathsFoundAll = {};
-let svgPathsFound = {};
+let svgPathsFoundObject = {};
+let fileNameArray = [];
 //use to test
 getSVGData('./svgs/1.svg');
 getSVGData('./svgs/2.svg');
-//console.log(pathsFoundAll);
-console.log(svgPathsFound);
+getPathByFilename('./svgs/1.svg');
 function getSVGData(svgFileName) {
-    svgPathsFound[svgFileName] = {};
+    //svgPathsFound[svgFileName]=[];
+    //let paths={};
     let paths = [];
     let path = extract(svgFileName);
     let svg = parse(path);
     console.log(svgFileName);
-    //console.log(svg);
     let pathString = "";
     let key = "";
     let x = 0;
     let y = 0;
+    //let count=0;
     for (let z = 0; z < svg.length; z++) {
         key = svg[z][0];
         //Handle new path
         if (z > 1 && (key == 'm' || key == "M")) {
             //Char M or m signals start of new path
-            // console.log(pathString);
-            pathsFoundAll[pathString] = pathsFoundAll[pathString] + 1 || 1;
             paths.push(pathString);
             pathString = "";
         }
@@ -47,10 +45,17 @@ function getSVGData(svgFileName) {
     }
     //Handle finish of path data
     //  console.log(pathString);
-    pathsFoundAll[pathString] = pathsFoundAll[pathString] + 1 || 1;
     paths.push(pathString);
-    svgPathsFound[svgFileName] = paths;
+    svgPathsFoundObject[svgFileName] = paths;
     pathString = "";
+}
+function getPathByFilename(fileName) {
+    let pathObject = svgPathsFoundObject[fileName];
+    Object.entries(pathObject).forEach((svg) => {
+        console.log();
+        console.log("path");
+        console.log(svg[1]);
+    });
 }
 //SVG Structure for reference
 // M = moveto
